ec0a48f172f316e0d28f130a81a0ec572df3c14b
Junio Silva <juniosilva.mr11@gmail.com>
2026-02-03 20:40:27 -0300

    Primeira versão das ultimas classes do Skill Up criadas.

---
+++ skillup_project/src/skillup_project/candidatura.py
@@ -0,0 +1,6 @@
+class Candidatura():
+    def __init__(self, id_candidatura, id_vaga, id_candidato, status):
+        self.id_candidatura = id_candidatura
+        self.id_vaga = id_vaga
+        self.id_candidato = id_candidato
+        self.status = status
--- skillup_project/src/skillup_project/instituicao_ensino.py
+++ skillup_project/src/skillup_project/instituicao_ensino.py
@@ -4,3 +4,15 @@ class InstituicaoEnsino(EntidadePublicadora):
     def __init__(self, id_instituicao, nome, cnpj, credenciada):
         super().__init__(id_instituicao, nome, cnpj)
         self.credenciada = credenciada 
+        
+class AreaEnsino:
+    def __init__(self, id_area, nome_area):
+        self.id_area = id_area
+        self.nome_area = nome_area
+        
+
+class InstituicaoAreaEnsino:
+    def __init__(self, id_instituicao_area, id_instituicao, id_area):
+        self.id_instituicao_area = id_instituicao_area
+        self.id_instituicao = id_instituicao
+        self.id_area = id_area
