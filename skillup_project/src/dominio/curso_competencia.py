09eca32f378618aa11c2c169c0a4520a34f2eee4
Junio Silva <juniosilva.mr11@gmail.com>
2026-02-25 20:31:46 -0300

    Refatoração: Criando arquivos separados para classes diferentes seguindo o SRP

---
+++ skillup_project/src/dominio/curso_competencia.py
@@ -0,0 +1,5 @@
+class CursoCompetencia:
+    def __init__(self, curso, competencia, nivel_conferido):
+        self.curso = curso
+        self.competencia = competencia
+        self.nivel_conferido = nivel_conferido # Ex: 1-Básico, 2-Intermediário
